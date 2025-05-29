668c69dd088a324a2e441f06049b5a3fe33a0c46
DaniloSovano <danilosovano@gmail.com>
2025-05-29 10:43:02 -0300

    Update conta.h

--- Aula2/conta.h
+++ Aula2/conta.h
@@ -1,1 +1,15 @@
 
+
+typedef struct {
+  int numero;
+  float saldo;
+
+} Conta;
+
+Conta inicializar(Conta nome);
+
+void verSaldo(Conta nome);
+
+void deposito(Conta *nome);
+void saque(Conta *nome);
+  
